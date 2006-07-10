dc7df6c1e08cea3d119b3add4766166c2815b46d
msilva <msilva@4e3d48d7-5912-0410-a75c-d88df2f7de60>
2006-07-10 17:36:53 +0000

    Added some function required to make public some dotProject's functions as Web Services.

---
+++ ws/tasks.test.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * Copyright (C) 2006 magsilva
+ */
+
+require_once("tasks.php");
+dotproject_initialize();
+user_initialize();
+user_login("admin", "admin");
+require_once($baseDir . "/classes/date.class.php");
+
+// prepare time period for 'events'
+$startPeriod = new CDate(Date_calc::beginOfWeek($dd, $mm, $yy, FMT_TIMESTAMP_DATE, LOCALE_FIRST_DAY));
+$startPeriod->setTime(0, 0, 0);
+$startPeriod->subtractSeconds( 1 );
+$endPeriod = new CDate(Date_calc::endOfWeek($dd, $mm, $yy, FMT_TIMESTAMP_DATE, LOCALE_FIRST_DAY));
+$endPeriod->setTime(23, 59, 59);
+ 
+$company_id = 1;
+$tasks = getTasksForPeriod($startPeriod, $endPeriod, $company_id);
+
+var_dump($tasks);
+?>
