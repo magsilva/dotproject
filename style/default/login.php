2c59e106c068f8d9c790383640669d3504b30603
msilva <msilva@4e3d48d7-5912-0410-a75c-d88df2f7de60>
2006-10-19 14:28:17 +0000

    - Removed the duplicated <title>. - Removed the duplicated stylesheet definition. - Removed the hidden fields from the table. - Removed the message about MySQL (we are using Adodb, it's not our problem if it doesn't support a specific MySQL version). - Removed the message about old PHP version.

--- style/default/login.php
+++ style/default/login.php
@@ -1,28 +1,27 @@
 <?php /* STYLE/DEFAULT $Id: login.php,v 1.32 2005/03/31 20:11:15 gregorerhardt Exp $ */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title><?php echo $dPconfig['page_title'];?></title>
+   	<title><?php echo $dPconfig['page_title'];?> :: <?php echo $dPconfig['company_name'];?> :: Login </title>
 	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
-       	<title><?php echo $dPconfig['company_name'];?> :: dotProject Login</title>
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta name="Version" content="<?php echo @$AppUI->getVersion();?>" />
 	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
-	<style type="text/css" media="all">@import "./style/<?php echo $uistyle;?>/main.css";</style>
 	<link rel="shortcut icon" href="./style/<?php echo $uistyle;?>/images/favicon.ico" type="image/ico" />
 </head>
 
 <body bgcolor="#f0f0f0" onload="document.loginform.username.focus();">
+
 <br /><br /><br /><br />
+
 <?php //please leave action argument empty ?>
-<!--form action="./index.php" method="post" name="loginform"-->
 <form method="post" action="<?php echo $loginFromPage; ?>" name="loginform">
-<table align="center" border="0" width="250" cellpadding="6" cellspacing="0" class="std">
 <input type="hidden" name="login" value="<?php echo time();?>" />
 <input type="hidden" name="lostpass" value="0" />
 <input type="hidden" name="redirect" value="<?php echo $redirect;?>" />
+
+<table align="center" border="0" width="250" cellpadding="6" cellspacing="0" class="std">
 <tr>
 	<th colspan="2"><em><?php echo $dPconfig['company_name'];?></em></th>
 </tr>
@@ -51,11 +50,6 @@
 <div align="center">
 <?php
 	echo '<span class="error">'.$AppUI->getMsg().'</span>';
-
-	$msg = '';
-	$msg .=  phpversion() < '4.1' ? '<br /><span class="warning">WARNING: dotproject is NOT SUPPORT for this PHP Version ('.phpversion().')</span>' : '';
-	$msg .= function_exists( 'mysql_pconnect' ) ? '': '<br /><span class="warning">WARNING: PHP may not be compiled with MySQL support.  This will prevent proper operation of dotProject.  Please check you system setup.</span>';
-	echo $msg;
 ?>
 </div>
 <center><?php echo "* ".$AppUI->_("You must have cookies enabled in your browser"); ?></center>
