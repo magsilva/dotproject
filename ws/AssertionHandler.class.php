356c06d11dd83d5f8784c7444a675de97f2bb947
msilva <msilva@4e3d48d7-5912-0410-a75c-d88df2f7de60>
2006-11-08 17:37:24 +0000

    - Fully documented the AssertionHandler.class.php.

--- ws/AssertionHandler.class.php
+++ ws/AssertionHandler.class.php
@@ -14,24 +14,71 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-Copyright (C) 2006 Marco Aurélio Graciotto Silva <magsilva@gmail.com>
+Copyright (C) 2006 Marco Aurelio Graciotto Silva <magsilva@gmail.com>
 */
 
+/**
+ * Base class for AssertionHandler.
+ */
 require_once('IssueHandler.class.php');
+
+/**
+ * File that defines the AssertException class (required by the
+ * {@link assert_handler()}).
+ */
 require_once('Exception.class.php');
 
+/**
+ * Convert any assertion failure into an exception.
+ * 
+ * Convert any failed assertion into an exception and throw is. This is
+ * achieved by implementing a new assertion handler.
+ * 
+ * @author Marco Aurelio Graciotto Silva
+ * @license GPL
+ * @since November/2006
+ * @package FailureHandler
+ */
 class AssertionHandler extends IssueHandler
 {
+	/**
+	 * Previous assertion checking state.
+	 * @var bool
+	 */
 	private $assert_active;
 	
+	/**
+	 * Previous assertion reporting state.
+	 * @var bool
+	 */
 	private $assert_warning;
 	
+	/**
+	 * Previous assertion bailing out state.
+	 * @var bool
+	 */
 	private $assert_bail;
 	
+	/**
+	 * Previous assertion quite evaluation state.
+	 * @var bool
+	 */
 	private $assert_quiet_eval;
 	
+	/**
+	 * Previous assertion handler function.
+	 * @var mixed
+	 */
 	private $assert_handler;
 	
+	/**
+	 * Initialize the AssertionHandler.
+	 * 
+	 * Initialize the AssertionHandler. The assertion mechanism is enabled,
+	 * any action that could result into application output (verbosity) is
+	 * disabled. Assertion failure bailing out is disabled (after all, the
+	 * assert exception wil cause the same effect).
+	 */
 	public function __construct()
 	{
 		// Enable assertions.
@@ -49,8 +96,6 @@ class AssertionHandler extends IssueHandler
 
 	public function __destruct()
 	{
-		parent::__destruct();
-		
 		// Restore assert options.
 		assert_options(ASSERT_ACTIVE, $this->assert_active);
 		assert_options(ASSERT_WARNING, $this->assert_warning);
@@ -60,22 +105,21 @@ class AssertionHandler extends IssueHandler
 	}
 		
 	/**
+	 * Assertion handler.
+	 * 
 	 * The assert_options() function and/or ASSERT_CALLBACK configuration
 	 * directive allow a callback function to be set to handle failed
 	 * assertions.
 	 * 
-	 * Assert() callbacks are particularly useful for building automated test
-	 * suites because they allow you to easily capture the code passed to the
-	 * assertion, along with information on where the assertion was made. While
-	 * this information can be captured via other methods, using assertions
-	 * makes it much faster and easier!
-	 * 
-	 * @param $file The file the assertion failed in.
-	 * @param $line The line the assertion failed on.
-	 * @param $code Contain the expression that failed (if any - literal values
+	 * @param string $file The file the assertion failed in.
+	 * @param int $line The line the assertion failed on.
+	 * @param mixed $code The expression that failed (if any - literal values
 	 * such as 1 or "two" will not be passed via this argument)
+	 * 
+	 * @throws AssertException Throws the exception when ever an assertion
+	 * fails.
 	 */
-	public function assertionHandler($file, $line, $code)
+	public function assertion_handler($file, $line, $code)
 	{
 		$context = $this->createReport();
 		throw new AssertException($file, $line, $code, $context);
