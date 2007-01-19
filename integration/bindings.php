8fce828030d7fba20fed6171c60ada887e338830
msilva <msilva@4e3d48d7-5912-0410-a75c-d88df2f7de60>
2007-01-19 16:16:56 +0000

    - Moving some files around.

--- integration/bindings.php
+++ integration/bindings.php
@@ -1,22 +1,40 @@
 <?php
 /*
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Copyright (C) 2006 Marco Aurelio Graciotto Silva <magsilva@gmail.com>
+*/
+
+
+/**
+ * We must handle the differences between PHP 4 and 5. The object
+ * model from those version is completely different and, as you know,
+ * IDEAIS is developement using OO (object-oriented) features.
  * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- * 
- * Copyright (C) 2006 magsilva
+ * The easiest solution is to adopt completely different code for PHP 4
+ * and PHP 5. The trend is that PHP 4, already obsoleted, come to desuse
+ * and any important PHP application be migrated to a newer (>= 5.0.0)
+ * version).
  */
-
-
-if (version_compare(phpversion(), "5.0.0", "<=")) {
-	require(dirname(__FILE__) . '/php4/bindings.php');	
-} else {
-	require(dirname(__FILE__) . '/php4/bindings.php');	
+ 
+function __autoload($class_name)
+{
+	if (version_compare(phpversion(), "5.0.0", ">=")) {
+		require_once(dirname(__FILE__) . '/php5/' . $class_name . '.class.php');
+	} else {
+		require_once(dirname(__FILE__) . '/php4/' . $class_name . '.class.php');
+	}	
 }	
 ?>
